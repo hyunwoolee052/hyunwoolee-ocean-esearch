b13624a0fc61b7f86a7e737f279701cfd2f138da
Hyunwoo Lee <hyunwoo9174@outlook.com>
2025-06-27 20:17:10 +0900

    refactor fetch_data.py to initialize data dictionary with required parameters

--- src/ocean/fetch_data.py
+++ src/ocean/fetch_data.py
@@ -2,5 +2,9 @@ import urllib.parse
 import urllib.request
 
 data = {}
-
+data["id"] = "sooList"
+data["key"] = "qPwOeIrU-2506-KUVBOC-1211"  # expired 2026-06-25
+data["sdate"] = "19620101"  # YYYYMMDD
+data["edate"] = "20241231"  # YYYYMMDD
 url_values = urllib.parse.urlencode(data)
+url = f"https://www.nifs.go.kr/bweb/OpenAPI_json"
